a122d98c3f4e9cadfa320d92434a1e47ca58558c
Siqi Li <siqi.li@shopee.com>
2021-09-04 15:13:12 +0800

    keyword notify

--- src/keywordNotify.js
+++ src/keywordNotify.js
@@ -1,7 +1,38 @@
-const { Wechaty } = require('wechaty') // import { Wechaty } from 'wechaty'
+const { Wechaty } = require('wechaty')
 
-Wechaty.instance() // Global Instance
-.on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`))
-.on('login',            user => console.log(`User ${user} logged in`))
-.on('message',       message => console.log(`Message: ${message}`))
-.start()
+const bot = new Wechaty()
+
+const KEYWORDS = ['回春丹'];
+
+const isIncludeKeyword = (message, keywords) => {
+  return keywords.some(keyword => message.includes(keyword))
+}
+
+const onMessage = async (message) => {
+  if (message.self()) { // Don't deal with message from yourself.
+    return;
+  }
+
+  const room = message.room();
+  if (!room) { // Don't deal with message outside a room
+    return;
+  }
+
+  const messageText = message.text();
+
+  if (isIncludeKeyword(messageText, KEYWORDS)) {
+    const roomName = await room.topic();
+    const self = await bot.Contact.find('又一')
+    await self.say(`${roomName}: ${messageText}`)
+    // await bot.say(`${roomName}: ${messageText}`)
+  }
+}
+
+bot.on('scan', (qrcode, status) => {
+    console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`)
+  })
+  .on('login', user => {
+    console.log(`User ${user} logged in`)
+  })
+  .on('message', onMessage)
+  .start()
